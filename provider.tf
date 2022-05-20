a01fc05937a2bbac62a5d8e6fd42539fba0ffdbf
Pedro <you@example.com>
2022-05-20 20:20:40 +0200

    add provider.tf

---
+++ provider.tf
@@ -0,0 +1,7 @@
+provider "azurerm" {
+  tenant_id       = "07160c00-0252-46d1-8d5a-2710f7cb9632"
+  subscription_id = "c117693e-630b-41b6-8f39-155ca9c44228"
+  client_id       = "a65fe665-5438-4c7c-8afa-b6017088f1b6"
+  client_secret   = "zzpfDguAquNYuG6MAgiD11rr.jebcSOj63"
+  features {}
+}
