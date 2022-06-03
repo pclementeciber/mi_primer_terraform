6e99841ab93518be4e722c9e36ac5eb26d1faff0
Pedro <you@example.com>
2022-06-03 19:36:21 +0200

    arreglo de IP

--- instances.tf
+++ instances.tf
@@ -1,5 +1,6 @@
 resource "azurerm_public_ip" "http" {
-  name                = "Frontal-IP"
+  for_each = var.array_de_vm
+  name                = "${each.key}-IP"
   location            = azurerm_resource_group.Ciber-Terraform.location
   resource_group_name = azurerm_resource_group.Ciber-Terraform.name
   allocation_method   = "Static"
@@ -9,15 +10,16 @@ resource "azurerm_public_ip" "http" {
 }
 
 resource "azurerm_network_interface" "http" {
-  name                = "Frontal-nic"
+  for_each = var.array_de_vm
+  name                = "${each.key}-nic"
   location            = azurerm_resource_group.Ciber-Terraform.location
   resource_group_name = azurerm_resource_group.Ciber-Terraform.name
 
   ip_configuration {
-    name                          = "Terraformconfig1"
+    name                          = "Terraform${each.key}"
     subnet_id                     = azurerm_subnet.http.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.http.id
+    public_ip_address_id          = azurerm_public_ip.http["${each.key}"].id
   }
 }
 
@@ -26,7 +28,7 @@ resource "azurerm_virtual_machine" "http" {
   name                  = "${each.key}"
   location              = azurerm_resource_group.Ciber-Terraform.location
   resource_group_name   = azurerm_resource_group.Ciber-Terraform.name
-  network_interface_ids = [azurerm_network_interface.http.id]
+  network_interface_ids = [azurerm_network_interface.http["${each.key}"].id]
   vm_size               = "Standard_DS1_v2"
 
   delete_os_disk_on_termination    = true
@@ -40,13 +42,13 @@ resource "azurerm_virtual_machine" "http" {
   }
 
   storage_os_disk {
-    name              = "Frontal-osdisk1"
+    name              = "${each.key}-osdisk1"
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
   }
 
   os_profile {
-    computer_name  = "Frontal"
+    computer_name  = "${each.key}"
     admin_username = "ubuntu"
     admin_password = "Dockerdocker1234!"
     ##custom_data    = file("scripts/first-boot.sh")
