18da121aff03f8b76c29b2b5f724e2a9e0e475bc
Pedro <you@example.com>
2022-06-03 20:17:02 +0200

    add tf to blob

--- version.tf
+++ version.tf
@@ -1,3 +1,9 @@
 terraform {
   required_version = ">= 1.0"
+  backend "azurerm" {
+    resource_group_name  = "tfstate"
+    storage_account_name = "tfstate2135r"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  } 
 }
